e5c5499994441bd64d39d051ff9ac5863312f23b
Brian Payne <bwpayne@gmail.com>
2013-05-25 23:00:52 -0700

    Added notification to the PushOver service Added geotools to notify with geoip information

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Finder\Finder;
 
+$config = require __DIR__.'/config/config.php';
+
 $app = new Silex\Application();
 $app['debug'] = false;
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
@@ -14,6 +16,26 @@ $app->register(new \Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new \Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/silex.log'
 ));
+$app['monolog'] = $app->share($app->extend('monolog', function($monolog, $app) use ($config) {
+    $monolog->pushHandler(new \Monolog\Handler\PushoverHandler(
+        $config['pushover_api_key'],
+        $config['pushover_user_key'],
+        'seedbox',
+        \Symfony\Bridge\Monolog\Logger::NOTICE
+    ));
+    return $monolog;
+}));
+$app->register(new \Geotools\Silex\GeotoolsServiceProvider());
+$app['geocoder'] = $app->share(function() {
+    $geocoder = new \Geocoder\Geocoder();
+    $adapter  = new \Geocoder\HttpAdapter\CurlHttpAdapter();
+    $geocoder->registerProviders(array(
+        new \Geocoder\Provider\FreeGeoIpProvider($adapter),
+        new \Geocoder\Provider\GeoipProvider($adapter)
+    ));
+    return $geocoder;
+});
+
 $app['basePath'] = __DIR__.'/data/';
 
 $app->get('/', function() use ($app) {
@@ -21,7 +43,6 @@ $app->get('/', function() use ($app) {
 });
 
 $app->get('/list{path}', function(Request $request, $path) use ($app) {
-    $app['logger']->info($request->getClientIp());
 
     if ($path == '/') {
         $path = '';
@@ -39,6 +60,13 @@ $app->get('/list{path}', function(Request $request, $path) use ($app) {
 })->value('path', '')->assert('path','.+')->bind('list');
 
 $app->get('/download{path}', function(Request $request, $path) use ($app) {
+    $ipAddress = $request->getClientIp();
+    $results = $app['geotools']->batch($app['geocoder'])->geocode($ipAddress)->parallel();
+    $result = reset($results);
+    $country = $result->getCountry();
+    $region = $result->getRegion();
+    $city = $result->getCity();
+    $app['logger']->notice("Download - [$ipAddress|$country|$region|$city] - $path");
     if (file_exists($app['basePath'].$path)) {
         return $app->sendFile($app['basePath'].$path, 200,array(),'attachment');
     } else {
